7f2141a69272a9e6135f3fd2a66c6c77be3578ae
birawaich <27787788+birawaich@users.noreply.github.com>
2024-10-07 21:31:46 +0800

    add V1 part

--- run.py
+++ run.py
@@ -5,13 +5,14 @@ import time
 import demo
 
 # load pipe
-# pipe_v1_small = pipeline(task="depth-estimation", model="LiheYoung/depth-anything-small-hf")
-# pipe_v2_small = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Small-hf")
-pipe_v2_large = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Large-hf")
+# pipe_v1 = pipeline(task="depth-estimation", model="LiheYoung/depth-anything-small-hf")
+pipe_v2_small = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Small-hf")
+# pipe_v2_large = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Large-hf")
 
 # load image
 # Specify the directory containing the images
 directory = "./samples"
+filename ="tianjin_orig.JPG"
 
 # List all files in the directory
 files = os.listdir(directory)
@@ -19,7 +20,10 @@ files = os.listdir(directory)
 assert len(files) > 0, "cannot have an empty `./samples/` directory!"
 
 #get first image
-first_image_path = os.path.join(directory, files[0])
+if filename != '':
+    first_image_path = directory+'/'+filename
+else:
+    first_image_path = os.path.join(directory, files[0])
 image = Image.open(first_image_path)
 image.show()
 
@@ -28,10 +32,10 @@ image.show()
 # depth_v1 = pipe_v1(image)["depth"]
 # end_time = time.time()
 # print(f"Estimated Depth using V1:\t {end_time-start_time: .2f}s")
-# depth_v1.show()
+# demo.grayscale_to_gradient(depth_v1).show()
 
 start_time = time.time()
-depth_v2= pipe_v2_large(image)["depth"]
+depth_v2= pipe_v2_small(image)["depth"]
 end_time = time.time()
 print(f"Estimated Depth using V1:\t {end_time-start_time: .2f}s")
 depth_v2.show()
