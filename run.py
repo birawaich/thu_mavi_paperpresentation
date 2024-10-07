de76ca798c8a8d14286239ccc083bd4819216b8a
birawaich <27787788+birawaich@users.noreply.github.com>
2024-10-07 23:42:22 +0800

    add remaining presentation

--- run.py
+++ run.py
@@ -5,14 +5,14 @@ import time
 import demo
 
 # load pipe
-# pipe_v1 = pipeline(task="depth-estimation", model="LiheYoung/depth-anything-small-hf")
+pipe_v1_small = pipeline(task="depth-estimation", model="LiheYoung/depth-anything-small-hf")
 pipe_v2_small = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Small-hf")
 # pipe_v2_large = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Large-hf")
 
 # load image
 # Specify the directory containing the images
 directory = "./samples"
-filename ="tianjin_orig.JPG"
+filename ="sbb_uw.JPG"
 
 # List all files in the directory
 files = os.listdir(directory)
@@ -28,15 +28,14 @@ image = Image.open(first_image_path)
 image.show()
 
 # inference
-# start_time = time.time()
-# depth_v1 = pipe_v1(image)["depth"]
-# end_time = time.time()
-# print(f"Estimated Depth using V1:\t {end_time-start_time: .2f}s")
-# demo.grayscale_to_gradient(depth_v1).show()
+start_time = time.time()
+depth_v1 = pipe_v1_small(image)["depth"]
+end_time = time.time()
+print(f"Estimated Depth using V1:\t {end_time-start_time: .2f}s")
+demo.grayscale_to_gradient(depth_v1).show()
 
 start_time = time.time()
 depth_v2= pipe_v2_small(image)["depth"]
 end_time = time.time()
-print(f"Estimated Depth using V1:\t {end_time-start_time: .2f}s")
-depth_v2.show()
+print(f"Estimated Depth using V2:\t {end_time-start_time: .2f}s")
 demo.grayscale_to_gradient(depth_v2).show()
